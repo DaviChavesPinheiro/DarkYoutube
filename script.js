5ba1479c2efea236f6cf37750f5df81ffcd42aec
Davi <davicpinheiro123@hotmail.com>
2020-03-22 10:26:54 -0300

    Arquivos adicionados. Icones adicionados.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let button
+
 window.onload = function(){
-    
+    const buttons = document.querySelector("#buttons")
+    if(!buttons) return
+
+    button = createButton()
+}
+
+function createButton(){
+    const button = document.createElement("button")
+    button.classList.add("dyt")
+    button.classList.add("button")
+    button.textContent = "Dark"
+    button.addEventListener("click", changeColor)
+    document.querySelector("#buttons").prepend(button)
+    return button
+}
+
+function changeColor() {
+    document.querySelector("#content").classList.toggle("dark")
 }
